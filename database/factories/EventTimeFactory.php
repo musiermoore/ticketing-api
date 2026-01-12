3b0a64e5d53541f8465895f18ff6e8e16e6903aa
Alexander Sustavov <markowillz@gmail.com>
2026-01-12 14:25:05 +0600

    Create EventTime model and factory

---
+++ database/factories/EventTimeFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\EventTime>
+ */
+class EventTimeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'date' => $this->faker->dateTimeBetween('now', '+3 months')->format('Y-m-d'),
+
+            // Event-friendly times (not 03:17 AM)
+            'time' => $this->getTime(),
+
+            'ticket_count' => $this->faker->numberBetween(0, 500),
+        ];
+    }
+
+    private function getTime()
+    {
+        $hours = $this->faker->numberBetween(8, 22);
+        $minutes = $this->faker->randomElement([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]);
+
+        return sprintf(
+            '%02d:%02d:00',
+            $hours,
+            $minutes
+        );
+    }
+}
---
+++ database/migrations/2026_01_12_073707_create_event_times_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_times', function (Blueprint $table) {
+            $table->id();
+
+            $table->date('date');
+            $table->time('time');
+            $table->integer('ticket_count')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_times');
+    }
+};
